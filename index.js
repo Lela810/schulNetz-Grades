26aa8b8b3a50a77b136dcc5f5eb7619ad84d344b
Lela810 <lklaus@online.gibz.ch>
2022-05-15 17:41:13 +0200

    Update

--- index.js
+++ index.js
@@ -12,7 +12,12 @@
     const container = new LiveContainer().hook();
 
 
-    connect(`mongodb://${process.env.MONGODB}/schulNetz-grades`, { useNewUrlParser: true })
+    if (!process.env.MONGODB_USERNAME || !process.env.MONGODB_PASSWORD) {
+        connect(`mongodb://${process.env.MONGODB}/schulNetz-grades`, { useNewUrlParser: true })
+    } else {
+        connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB}/schulNetz-grades`, { useNewUrlParser: true })
+    }
+
     const db = connection
     db.on('error', (error) => console.error(error))
     db.once('open', () => console.log('Connected to Database'))
